6a744d00a9bfefc6dabdc2c6bbb9a382d38d1c96
anshikasx <anshikasinha1805@gmail.com>
2026-06-10 10:37:57 +0530

    Add dynamic port for deployment

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,8 @@ app.use(express.json());
 app.use("/api/movies", movieRoutes);
 app.use("/api/bookings", bookingRoutes);
 
-app.listen(5001, () => {
-  console.log("Server running on port 5001");
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
